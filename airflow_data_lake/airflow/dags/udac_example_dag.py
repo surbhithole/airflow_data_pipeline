7aa09d407408bec8bcbf489f26dca25f34a9d6d5
Surbhi Thole <sst390@nyu.edu>
2020-12-03 18:56:49 -0500

    Update udac_example_dag.py

--- airflow_data_lake/airflow/dags/udac_example_dag.py
+++ airflow_data_lake/airflow/dags/udac_example_dag.py
@@ -17,6 +17,7 @@ default_args = {
     'email_on_retry'  : False,
     'retries' : 3,
     'retry_delay' : timedelta(minutes = 5),
+    'catchup' : False,
 }
 
 dag = DAG('udac_example_dag',
@@ -102,14 +103,8 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> stage_events_to_redshift >> load_songplays_table
-start_operator >> stage_songs_to_redshift >> load_songplays_table
-
-
-load_songplays_table >> load_song_dimension_table >> run_quality_checks
-load_songplays_table >> load_user_dimension_table >> run_quality_checks
-load_songplays_table >> load_artist_dimension_table >> run_quality_checks
-load_songplays_table >> load_time_dimension_table >> run_quality_checks
-
-
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table]
+[load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
 run_quality_checks >> end_operator
